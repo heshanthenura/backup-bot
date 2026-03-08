d6b5def74f870ce32560fe9a62bc1004c05f8cf4
Heshan Thenura Kariyawasam <75155192+heshanthenura@users.noreply.github.com>
2026-03-08 18:46:18 +0000

    refac: switched to http1

--- upload_backup.sh
+++ upload_backup.sh
@@ -7,6 +7,7 @@ curl -X POST https://content.dropboxapi.com/2/files/upload \
   --header "Authorization: Bearer $ACCESS_TOKEN" \
   --header "Dropbox-API-Arg: {\"path\": \"/backups/$ZIP_FILE\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}" \
   --header "Content-Type: application/octet-stream" \
+  --http1.1 \
   --data-binary @"$ZIP_FILE"
 
 echo "Backup uploaded: $ZIP_FILE"
