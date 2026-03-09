2b40c9029436937a173c54961db76cc474825a12
Heshan Thenura Kariyawasam <75155192+heshanthenura@users.noreply.github.com>
2026-03-09 19:36:06 +0000

    testttt

--- upload_backup.py
+++ upload_backup.py
@@ -1,21 +1,28 @@
 import os
+import sys
 import dropbox
 
-
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
-ZIP_FILE = os.environ.get("ZIP_FILE")
 
 if not ACCESS_TOKEN:
     raise ValueError("ACCESS_TOKEN is not set")
-if not ZIP_FILE or not os.path.exists(ZIP_FILE):
-    raise ValueError(f"ZIP_FILE '{ZIP_FILE}' does not exist")
+
+if len(sys.argv) < 2:
+    raise ValueError("Usage: python upload.py <file_path>")
+
+FILE_PATH = sys.argv[1]
+
+if not os.path.exists(FILE_PATH):
+    raise ValueError(f"File '{FILE_PATH}' does not exist")
+
+FILE_NAME = os.path.basename(FILE_PATH)
 
 dbx = dropbox.Dropbox(ACCESS_TOKEN)
 
-dest_path = f"/backups/{ZIP_FILE}"
+dest_path = f"/backups/{FILE_NAME}"
 
-with open(ZIP_FILE, "rb") as f:
-    print(f"Uploading {ZIP_FILE} to Dropbox at {dest_path}...")
+with open(FILE_PATH, "rb") as f:
+    print(f"Uploading {FILE_NAME} to Dropbox at {dest_path}...")
     dbx.files_upload(f.read(), dest_path, mode=dropbox.files.WriteMode.add)
 
 print("Upload completed!")
